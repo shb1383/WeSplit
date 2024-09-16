59816eb12e40e3b39bf78e472fe3f7a59b6124ad
syrene <144994323+shb1383@users.noreply.github.com>
2024-09-16 12:36:57 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Syrene Haber Bartolome on 2024/09/16.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
